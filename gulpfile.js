7998237ea942bb7c2e8fc336146a90f83205eacf
Lou Fontana <loufontana@gmail.com>
2016-01-30 16:08:30 -0700

    updated all css colors to rgb

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var jsConcat = require('gulp-concat');
+var scss = require('gulp-sass');
 
-gulp.task('default', ['jsConcat', 'watch']);
+gulp.task('default', ['jsConcat', 'scssTranspile', 'watch']);
 
 gulp.task('jsConcat', function() {
   return gulp.src('./src/**/*.js')
@@ -9,6 +10,14 @@ gulp.task('jsConcat', function() {
     .pipe(gulp.dest('./public/script'));
 });
 
+gulp.task('scssTranspile', function() {
+  return gulp.src('./src/**/*.scss')
+    .pipe(scss('style.css'))
+    .pipe(gulp.dest('./public/style'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./src/**/*.js', ['jsConcat']);
+  gulp.watch('./src/**/*.scss', ['scssTranspile']);
+
 });
